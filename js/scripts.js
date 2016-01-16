2ba36bc5e287ac945bb828e0bb9c9f23097f92ab
Maile Lucks <maile.lucks@gmail.com>
2016-01-16 12:21:44 -0800

    take values from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
 var DEBUG_MODE = true;
 var debug = function(msg) {
     if (DEBUG_MODE == true) {
@@ -7,6 +5,40 @@ var debug = function(msg) {
     }
 }
 
+var params = {
+                
+            };
 
+            var result = $.ajax({
+                /* update API end point */
+                url: "https://api.foursquare.com/v2/venues/explore",
+                data: params,
+                dataType: "jsonp",
+                /*set the call type GET / POST*/
+                type: "GET",
+            })
+
+            /* if the call is successful (status 200 OK) show results */
+            .done(function (result) {
+                /* if the results are meeningful, we can just console.log them */
+                console.log(result);
+            })
+
+            /* if the call is NOT successful show errors */
+            .fail(function (jqXHR, error, errorThrown) {
+                console.log(jqXHR);
+                console.log(error);
+                console.log(errorThrown);
+            });
+
+$(document).ready(function() {
+$('.locationForm').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var locale = $(this).find("input[name='location']").val();
+		getSnacks(locale);
+	});
 
 });
